54aaf98bb9cef3f19fb8031c0d27a20b0a354c6a
Claire Dimitriuc <121368271+macaroonforu@users.noreply.github.com>
2024-01-21 16:30:31 -0500

    Update savvy_scooper.ino

--- savvy_scooper.ino
+++ savvy_scooper.ino
@@ -6,22 +6,21 @@ int enaA = 5;
 int in1 = 6;
 int in2 = 7;
 
-int enaB = 11;
+int enaB = 11; //10 got fried!
 int in3 = 8;
 int in4 = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  s1.attach(3);
-  //Motor A
+  //Attach a servo object to pin3 
+  s1.attach(3); 
+  //yellow wheel 
   pinMode(enaA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  //Motor B
+  //black wheel 
   pinMode(enaB, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-
   //IR Sensor
   pinMode(12, OUTPUT);
   pinMode(2, INPUT);
@@ -29,39 +28,36 @@ void setup() {
 }
 
 void loop(){
-    //CLOCKWISE MAX SPEED A&B for 2 seconds
-    start();
-    check();
-    delay(2000);
-    //STOP A to turn (Black)
+    start(); //Start the motors 
+    check(); 
+    delay(2000); //drive 2 seconds
+    //stop yellow wheel to turn 
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
-
     check();
-    delay(1000);
-
-    //Start A again so both are running
+    delay(1000); //drive 2 seconds
+  
+    //start yellow wheel again 
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     analogWrite(enaA, 127);
-   
     check();
-    delay(3000);
-    //STOP B to turn
+    delay(3000); //drive 3 seconds
+
+    //stop black wheel to turn
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
-   
     check();
-    delay(2000);
-    //Start B again so both are running
+    delay(2000); //drive 2 seconds
+  
+    //start black wheel again
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
     analogWrite(enaB, 127);
- 
     check();
-    //delay(2000);
 }
 
+//turn both motors off
 void stop(){
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
@@ -69,6 +65,7 @@ void stop(){
   digitalWrite(in4, LOW);
 }
 
+//start both motors 
 void start(){
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
@@ -78,32 +75,33 @@ void start(){
   analogWrite(enaB, 127);
 }
 
+//check if the IR sensor is being blocked or not and react accordingly 
 void check(){
+  //The IR sensor is being blocked: garbage detected!
   if(digitalRead(2) == LOW){
-      digitalWrite(12, HIGH);
-      //delay(10);
-      collect();
+      digitalWrite(12, HIGH); //turn red LED on 
+      collect(); //collect garbage 
   }
   else{
-    digitalWrite(12, LOW);
-    //delay(10);
+    digitalWrite(12, LOW); //turn red LED off
   } 
 }
 
+//Turn off motors, pick up garbage, wait for IR sensor to stop being blocked then resume motion 
 void collect(){
-    digitalWrite(12, HIGH);
     delay(10);
-    stop();
-    delay(1000);
-    s1.write(90);
-    delay(1000);
-    s1.write(0);
-    delay(1000);
+    stop(); //turn off both motors 
+    delay(1000); //wait 1 second
+    s1.write(90); //rotate scooper down 
+    delay(1000); //wait 1 second
+    s1.write(0); //rotate scooper back up 
+    delay(1000); //wait 1 second 
+
+   //Wait for the IR sensor to stop being obstructed 
     while(digitalRead(2)==LOW){
       delay(10);
       if(digitalRead(2)==HIGH){
         break;
       }
-    }
-    start();
+    start(); //turn motors back on 
 }
